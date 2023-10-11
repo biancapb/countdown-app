1fff631be94bff6f47cdf995692ea477ea4bb41f
Bianca Barbosa <yume_shiro@hotmail.com>
2023-10-11 00:29:38 -0300

    feat: creating useCountdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import Counter from "./components/Counter/Counter";
 import Title from "./components/Title/Title";
 import Photo from "./assets/festa-foto.jpg";
-import "./index.css";
 import "./App.css";
 
+import useCountdown from "./hooks/useCountdown";
+
 function App() {
+  const [day, hour, minute, second] = useCountdown("Jan 1, 2024 00:00:00");
   return (
     <>
       <div>
-        <div className="container" style={{ backgroundImage: `url(${Photo})` }}>
-          <Title title="contagem" />
-          <div className="countdown-container">
-            <Counter title="Dias" number={2} />
-            <Counter title="Horas" number={2} />
-            <Counter title="Minutos" number={2} />
-            <Counter title="Segundos" number={2} />
+        <div className="App" style={{ backgroundImage: `url(${Photo})` }}>
+          <div className="container">
+            <div className="countdown-title">
+              <Title title="Contagem Regressiva Ano 2024" />
+              <div className="countdown-container">
+                <Counter title="Dias" number={day} />
+                <Counter title="Horas" number={hour} />
+                <Counter title="Minutos" number={minute} />
+                <Counter title="Segundos" number={second} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
